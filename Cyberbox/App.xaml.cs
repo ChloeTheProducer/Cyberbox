56d8e2cd72240a7d9509c816531c08fcc47f3884
ChloeTheProducer <dragracer432@gmail.com>
2024-06-23 12:31:10 -0700

    and i oop

--- Cyberbox/App.xaml.cs
+++ Cyberbox/App.xaml.cs
@@ -8,7 +8,7 @@ namespace Cyberbox
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
     }
 }
