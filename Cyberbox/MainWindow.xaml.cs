5b3817ec0696a5f86d41807d0ebf1385e9284a6c
ChloeTheProducer <dragracer432@gmail.com>
2024-07-09 10:37:50 -0700

    trek stark

--- Cyberbox/MainWindow.xaml.cs
+++ Cyberbox/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualBasic;
 using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Media;
@@ -100,21 +101,6 @@ namespace Cyberbox
 
         // This will run the converter to export data to .wav depending on the designated show file type
         private void Export_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        // Drops down a menu to select different options
-        // (Note: Not sure if im gonna keep this because all this software does is
-        // take the show files and converts them to .wav. You dont really need any extra options for that.
-        // all you need is to import your file. If you accidentally selected the wrong format type for
-        // the show file, then all you need is to select import and redo the import
-        // cause trying to setup an auto determine to determine an exact stage type for the show
-        // would be difficult as one show file would/could work with multiple stages and shows.
-        // But i will setup an auto determine to generalize stage types for that format. So if you import
-        // a .cshw it will give you the stage options for that format so you just have to select if
-        // its for modern cybers or ptt cybers.)
-        private void Edit_Click(object sender, RoutedEventArgs e)
         {
 
         }
