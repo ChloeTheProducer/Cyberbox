b509ab5681548616001f44c355c3054f79a84dd7
ChloeTheProducer <dragracer432@gmail.com>
2024-10-09 22:08:29 -0400

    fixed button positions before working on UI

--- Assets/Scripts/UserManual.cs
+++ Assets/Scripts/UserManual.cs
@@ -3,20 +3,8 @@ using UnityEngine;
 
 public class UserManual : MonoBehaviour
 {
-    // Set the path to your PDF file here
-    public string pdfFilePath = "/home/chloe/User Manual.pdf";
-
-    // This method will be triggered when the button is clicked
-    public void OpenPDF()
+    private void OpenPDF()
     {
-        if (!string.IsNullOrEmpty(pdfFilePath))
-        {
-            // Open the PDF file with the default application
-            Process.Start(new ProcessStartInfo(pdfFilePath) { UseShellExecute = true });
-        }
-        else
-        {
-            UnityEngine.Debug.Log("PDF file path is not set or is invalid.");
-        }
+        Application.OpenURL(System.Environment.CurrentDirectory + "/User Manual.pdf");
     }
 }
